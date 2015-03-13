dc84188ee315021789e6ab4e303a0f036f4e3b55
Travis <travis@mcdem.us>
2015-03-12 22:51:46 -0400

    Make things work on debian

--- forge_history.tcl
+++ forge_history.tcl
@@ -30,18 +30,24 @@ if { [llength $messages] < 1 } {
 	exit 1
 }
 
-puts "How many days should be forged?"
-set numDays [read -nonewline stdin]
-puts "What's the minimum number of commits per day?"
-set minCommits [read -nonewline stdin]
-puts "What's the maximum number of commits per day?"
-set maxCommits [read -nonewline stdin]
+#puts "How many days should be forged?"
+#set numDays [read -nonewline stdin]
+set numDays 777
+#puts "What's the minimum number of commits per day?"
+#set minCommits [read -nonewline stdin]
+set minCommits 3
+#puts "What's the maximum number of commits per day?"
+#set maxCommits [read -nonewline stdin]
+set maxCommits 37
 set fileName "advice.txt"
 
 for {set i 0} {$i < $numDays} {incr i} {
-	set date [exec date -v -[expr $numDays - $i]d]
-	set numCommits [exec jot -r 1 $minCommits $maxCommits]
+	# set date [exec date -v-[expr $numDays - $i]d]
+	set date [exec date -d "-[expr $numDays - $i] days"]
+	#set numCommits [exec jot -r 1 $minCommits $maxCommits]
+	set numCommits [exec awk -vmin=$minCommits -vmax=$maxCommits {BEGIN{srand(); print int(min+rand()*(max-min+1))}}]
 	for {set j 0} {$j < $numCommits} {incr j} {
+		puts $j
 		set fortune [exec fortune]
 		set fh [open $fileName w+]
 		puts $fh $fortune
